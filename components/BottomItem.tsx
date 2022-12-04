8b4f9de02cb6158dad9f9ccc33d5a6a116c85544
filipkatusin <filip.katusin@student.tuke.sk>
2022-12-04 01:34:42 +0100

    added map zoom after click on user

--- components/BottomItem.tsx
+++ components/BottomItem.tsx
@@ -1,11 +1,11 @@
-const BottomItem = () => {
+const BottomItem = ({img, name, text, onClick }) => {
   return (
-    <li className='flex justify-between items-center w-full'>
-      <div className='flex'>
-        image
-        <div>
-          <h2>name</h2>
-          <p>text</p>
+    <li className='flex justify-between items-center w-full' onClick={() => onClick?.()}>
+      <div className='flex space-x-2 p-2'>
+        <img src={img} alt={name} className="w-14 h-14 rounded-full object-cover"/>
+        <div className="flex flex-col justify-center">
+          <h2 className="font-bold">{name}</h2>
+          <p className="text-sm">{text}</p>
         </div>
       </div>
 
@@ -13,13 +13,13 @@ const BottomItem = () => {
         xmlns='http://www.w3.org/2000/svg'
         fill='none'
         viewBox='0 0 24 24'
-        stroke-width='1.5'
-        stroke='currentColor'
-        className='w-6 h-6'
+        strokeWidth='3'
+        stroke='#2EBFA5'
+        className='w-8 h-10 rotate-90'
       >
         <path
-          stroke-linecap='round'
-          stroke-linejoin='round'
+          strokeLinecap='round'
+          strokeLinejoin='round'
           d='M4.5 15.75l7.5-7.5 7.5 7.5'
         />
       </svg>
--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,29 @@ import React, { useEffect, useState } from 'react';
 import { GoogleMap, LoadScript, useJsApiLoader } from '@react-google-maps/api';
 import { MarkerF } from '@react-google-maps/api';
 import BottomItem from '../components/BottomItem';
+import {log} from "util";
 
 const containerStyle = {
     width: '100vw',
     height: '100vh',
 };
 
-const center = {
-    lat: 48.731515,
-    lng: 21.243395,
-};
 
 function MyComponent() {
     const [markers, setMarkers] = useState([]);
     const [role, setRole] = useState("passenger");
+    const [center, setCenter] = useState({
+        lat: 48.731515,
+        lng: 21.243395,
+    })
+    const [zoom, setZoom] = useState(10);
+    const [modalOpen, setModalOpen] = useState(false);
 
     const fetchMarkers = async () => {
         const response = await fetch('http://localhost:3500/markers');
         const data = await response.json();
+
+        console.log(data)
 
         return data;
     };
@@ -36,7 +41,7 @@ function MyComponent() {
                 <GoogleMap
                     mapContainerStyle={containerStyle}
                     center={center}
-                    zoom={10}
+                    zoom={zoom}
                     options={{
                         zoomControl: false,
                         streetViewControl: false,
@@ -58,23 +63,35 @@ function MyComponent() {
                 </GoogleMap>
             </LoadScript>
 
-            <div className='absolute bottom-0 left-0 w-full bg-white p-3 rounded-t-2xl'>
-                <div className='flex flex-col items-center space-y-3'>
-                    <h2 className='font-semibold text-2xl'>Pick your ride</h2>
-                    <p className='text-gray-400 pb-4'>
-                        Select desired driver from the list below
-                    </p>
-                    <ul className='w-full'>
-                        <BottomItem />
+            <div className='absolute bottom-0 left-0 w-full bg-white p-3 pt-6 rounded-t-2xl group'>
+                <div className='flex flex-col items-center space-y-2'>
+                    <div className="text-center space-y-3" onClick={() => setModalOpen(prev => !prev)}>
+                        <h2 className='font-semibold text-2xl'>Pick your ride</h2>
+                        <p className='text-gray-400 pb-4'>
+                            Select desired driver from the list below
+                        </p>
+                    </div>
+                    <ul className={`w-full space-y-2 py-2 border-t border-gray-300 overflow-hidden max-h-0 transition-all ${modalOpen ? "max-h-[1000px]" : ""}`}>
+                        {markers?.map((marker, index) => {
+                            return <BottomItem key={index} img={marker?.user?.photo} name={marker?.user?.name} text={marker?.user?.distance} onClick={() => {
+                                setCenter({
+                                    lat: parseFloat(marker.lat),
+                                    lng: parseFloat(marker.lon),
+                                })
+                                setModalOpen(false)
+                                setZoom(16)
+                            }
+                            }/>
+                        })}
                     </ul>
                 </div>
-                <div className='flex justify-between items-center border-t border-gray-300 pt-4'>
-                    <div className='space-x-6 rounded bg-green-400 p-2'>
+                <div className='flex justify-between items-center  pt-4'>
+                    <div className='space-x-6 rounded-xl bg-green p-2'>
                         <button
                             onClick={() => setRole("passenger")}
                             className={`text-lg font-bold px-2 py-1 rounded-md transition-all ${
                                 role === "passenger"
-                                    ? 'bg-white text-green-400'
+                                    ? 'bg-white text-green'
                                     : 'text-white'
                             }`}
                         >
@@ -83,7 +100,7 @@ function MyComponent() {
                         <button
                             onClick={() => setRole("driver")}
                             className={`text-lg font-bold px-3 py-1 rounded-md transition-all ${
-                                role === "driver" ? 'bg-white text-green-400' : 'text-white'
+                                role === "driver" ? 'bg-white text-green' : 'text-white'
                             }`}
                         >
                             Driver
